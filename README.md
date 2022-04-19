84b812c93c15de2f239531460cc009183fae4e42
thiago jose da silva <thiago.ads.silva@gmail.com>
2022-04-18 22:02:07 -0300

    Criação do projeto e organização das estruturas de pasta junto com a criação do cabeçalho

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# ThiagoSiteConsultorioDentario
+# Projeto site 'Consultório Dentário'
+
+- Site para divulgação do trabalho de um consultório dentário, mostrando todos os serviços e suas avaliações conforme o depoimentos dos clientes.
+- Vou está usando as tecnologias:
+  - HTML
+  - CSS
+  - JAVASCRIPT
+  - BOOTSTRAP
+  - PHP
+  - MYSQL
+
+# WebSite (Consultório Dentário)
+
+- Site desenvolvido com o apoio do canal do youtube:
+
+  - Através do canal do youtube: https://www.youtube.com/channel/UCKwgH3vASrD2brd1l2m6NHw
+
+  - Vídeo completo: https://www.youtube.com/watch?v=0qu4fTFr26M&ab_channel=Mr.WebDesigner
+
+<hr>
+
+## Projeto Publicado: https://consultorioDentario.tfcoder.com/
+
+<hr>
+
+## Imagem do projeto
+
+![Tela Principal](images/01.PNG)
+
+<hr>
---
+++ frontend/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consultório Dentário</title>
+
+    <!--Importando as fonts do projeto-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap" rel="stylesheet">
+    <!--Todos os links foram tirados desse site: https://cdnjs.com/-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+    <!--Chamada do arquivo css-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!--INICIO DA PARTE DO HEADER-->
+    <header class="header fixed-top">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <a href="#home" id="home" class="logo">Dental<span>Care.</span></a>
+
+                <nav class="nav">
+                    <a href="#home" id="home">Inicio</a>
+                    <a href="#about" id="home">Sobre</a>
+                    <a href="#services" id="home">Serviços</a>
+                    <a href="#reviews" id="home">Avaliações</a>
+                    <a href="#contact" id="home">Contato</a>
+                </nav>
+
+                <a href="#contact" class="link-btn">Marcar Consulta</a>
+            </div>
+        </div>
+    </header>
+    <!--FIM DA PARTE DO HEADER-->
+
+
+
+    <!--Chamada do arquivo javascript-->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
